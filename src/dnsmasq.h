5c8802cbce03489712151d4e299143648f2ef564
Kevin Darbyshire-Bryant <kdbtomato@darbyshire-bryant.me.uk>
2014-12-23 17:44:11 +0000

    Revert "Use inotify instead of polling on Linux."
    This reverts commit 193de4abf59e49c6b70d54cfe9720fcb95ca2f71.

--- src/dnsmasq.h
+++ src/dnsmasq.h
@@ -542,10 +542,6 @@ struct resolvc {
   int is_default, logged;
   time_t mtime;
   char *name;
-#ifdef HAVE_LINUX_NETWORK
-  int wd; /* inotify watch descriptor */
-  char *file; /* pointer to file part if path */
-#endif
 };
 
 /* adn-hosts parms from command-line (also dhcp-hostsfile and dhcp-optsfile */
@@ -1003,7 +999,7 @@ extern struct daemon {
   /* DHCP state */
   int dhcpfd, helperfd, pxefd; 
 #if defined(HAVE_LINUX_NETWORK)
-  int netlinkfd, inotifyfd;
+  int netlinkfd;
 #elif defined(HAVE_BSD_NETWORK)
   int dhcp_raw_fd, dhcp_icmp_fd, routefd;
 #endif
@@ -1482,8 +1478,3 @@ void loop_send_probes();
 int detect_loop(char *query, int type);
 #endif
 
-/* inotify.c */
-#ifdef HAVE_LINUX_NETWORK
-void inotify_dnsmasq_init();
-int inotify_check(void);
-#endif
